26e613a84dd06998f26fbd2c5a9416d2ea8cf524
Jeffrey Lai <jeffrey_pro@hotmail.com>
2023-09-11 10:45:04 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+
+namespace SpeechToText
+{
+    internal class Program
+    {
+
+        async static Task FromMic(SpeechConfig speechConfig)
+        {
+            using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+            using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+            Console.WriteLine("Speak into your microphone.");
+            //while (true)
+            //{
+            //    Task.Delay(1000).Wait();
+            //}
+            var result = await speechRecognizer.RecognizeOnceAsync();
+            Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+        }
+
+        async static Task Main(string[] args)
+        {
+            var speechConfig = SpeechConfig.FromSubscription("9f76706cb0b74dfeb7b120508bd71966", "swedencentral");
+            await FromMic(speechConfig);
+        }
+    }
+}
